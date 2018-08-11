662b88fcf6a65360ccbd813de782b115748b1ad8
Jan van der Lugt <janlugt@gmail.com>
2018-08-11 15:43:45 -0700

    -

--- get_thumbnails.py
+++ get_thumbnails.py
@@ -31,16 +31,47 @@ def get_thumbnails():
   for item in playlist_items:
     title = item['snippet']['title']
     video_id = item['contentDetails']['videoId']
-    thumbnails = item['snippet']['thumbnails']
+    print(title)
 
-    best_thumbnail = max(thumbnails.values(), key=lambda item: item['width'])
+    # Folder
     ascii_title = title.encode('ascii', 'ignore').decode()
     ascii_title = re.sub(r'[/:]', '', ascii_title)
-    os.mkdir(ascii_title)
+    try:
+      os.mkdir(ascii_title)
+    except FileExistsError:
+      pass
+   
+    # Thumbnail
+    thumbnails = item['snippet']['thumbnails']
+    best_thumbnail = max(thumbnails.values(), key=lambda item: item['width'])
     urllib.request.urlretrieve(best_thumbnail['url'], '%s/thumbnail.jpg' % ascii_title)
+
+    # QR code
     url = pyqrcode.create('https://youtu.be/%s' % video_id)
     url.svg('%s/qr.svg' % ascii_title, scale=8)
-    print('%s %s' % (title, video_id))
+
+    # Comments
+    comments = []
+    try:
+      response = youtube.commentThreads().list(
+        part='snippet',
+        videoId=video_id,
+        textFormat='plainText',
+      ).execute()
+      comments = response['items']
+    except HttpError:
+      pass
+
+    # Text file
+    with open('%s/data.txt' % ascii_title, 'a') as data:
+      data.write('Title: %s\n' % title)
+      data.write('Published at: %s\n' % item['contentDetails']['videoPublishedAt'].split('T')[0])
+      data.write('Description: %s\n\n' % item['snippet']['description'])
+      data.write('Comments:\n\n')
+      for comment in comments:
+        data.write('%s\n' % comment['snippet']['topLevelComment']['snippet']['authorDisplayName'])
+        data.write('%s\n' % comment['snippet']['topLevelComment']['snippet']['textDisplay'])
+        data.write('\n')
 
 
 if __name__ == "__main__":
